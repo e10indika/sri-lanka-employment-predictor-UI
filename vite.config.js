7a04a8fe70e2a79d616d7a09b0ec5692939462e0
e10indika <indikaseeu@gmail.com>
2026-01-15 23:24:57 -0500

    Fix deploying issue

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/sri-lanka-employment-predictor-UI/',
   server: {
     host: '0.0.0.0',  // Allow external access
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://192.168.1.6:8000',
         changeOrigin: true,
       }
     }
